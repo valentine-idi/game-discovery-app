809c593b9fc167ccbc154c71dad84c5a5a8bd2a4
Valentine Idi <idi.valentine@gmail.com>
2025-07-03 19:56:15 +0300

    installed chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Card } from "@chakra-ui/react";
 function App() {
-  return <></>;
+  return (
+    <Card.Root>
+      <Card.Header>
+        <Card.Title>Game Discovery App</Card.Title>
+        <Card.Description>
+          Discover new games and share your favorites with the community.
+        </Card.Description>
+      </Card.Header>
+    </Card.Root>
+  );
 }
 
 export default App;
